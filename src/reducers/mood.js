41aed2bb3436a5426a9d9b80f173128b08a7a422
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2020-12-01 14:37:34 -0500

    refactor: Use createSlice to create the reducer

--- src/reducers/mood.js
+++ src/reducers/mood.js
@@ -1,18 +1,22 @@
-import { createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-export default createReducer(
-  {
-    anger: 0,
-    disgust: 0,
-    energy: 3,
-    fear: 0,
-    happy: 0,
-    pain: 0,
-    sad: 0,
-    surprise: 0,
-    tender: 0
-  },
-  builder => {
-    builder.addDefaultCase(state => { state.energy = 3 })
-  }
-);
+export const initialState = {
+  anger: 0,
+  disgust: 0,
+  energy: 3,
+  fear: 0,
+  happy: 0,
+  pain: 0,
+  sad: 0,
+  surprise: 0,
+  tender: 0
+}
+
+export const moodSlice = createSlice({
+  name: 'mood',
+  initialState,
+  reducers: {},
+  extraReducers: {}
+});
+
+export default moodSlice.reducer;
